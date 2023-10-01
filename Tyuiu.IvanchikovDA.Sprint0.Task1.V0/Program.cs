21b32c2e5156ef0a4352772f9f36ecf39dedc457
Tyuiu.IvanchikovDA@outlook.com <ivanchikov2022@internet.ru>
2023-10-01 14:04:32 +0500

    Task1

---
+++ Tyuiu.IvanchikovDA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.IvanchikovDA.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Дима";
+            int age = 22;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " года!");
+            Console.ReadKey();
+        }
+    }
+}
